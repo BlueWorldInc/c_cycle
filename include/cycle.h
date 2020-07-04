b6f2c4d43e4a261d7e2c65db76fecb73aae3db13
BlueWorldInc <keamybakunin@gmail.com>
2020-07-04 15:05:50 +0200

    first commit

---
+++ include/cycle.h
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+#include <math.h>
+#include <sdl_circle.h>
+#include <SDL2/SDL.h>
+
+#define SCREEN_WIDTH 1920
+#define SCREEN_HEIGHT 1080
+#define GROUND_Y SCREEN_HEIGHT * 0.75
